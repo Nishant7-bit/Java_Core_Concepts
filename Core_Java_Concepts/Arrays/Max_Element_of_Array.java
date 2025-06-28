e6af41d809d99c5d5c219d015769525a00f11f1e
nikobit <tripathinishant777@gmail.com>
2025-06-29 01:08:55 +0530

    Maximum Element From The Array

---
+++ Core_Java_Concepts/Arrays/Max_Element_of_Array.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Max_Element_of_Array{
+    public static void main(String[]args){
+        //Declare the Scanner Class Object
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the size of the Arrya");
+     //Enter the Size of the Array
+     int N = sc.nextInt();
+
+     //Declare the Array
+     int arr[] = new int[N];
+     System.out.println("Enter the array Elements");
+     
+     //Scanning User Inputs 
+    //if we do not scan the Input through
+    //for Loop it will print the empty array
+    //of respective input size    
+    for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();    
+            System.out.print(arr[i] + " ");
+        }
+    //Let suppose the Max Element if First
+        int max_element = arr[0];
+    //Find Max Elemnt from the List
+        for (int i = 1; i < N; i++){
+          
+            if (arr[i] > max_element){
+                max_element = arr[i];
+            }
+            
+        }
+        System.out.println( "This is the max Element " +max_element);
+        sc.close();
+
+    }
+}
